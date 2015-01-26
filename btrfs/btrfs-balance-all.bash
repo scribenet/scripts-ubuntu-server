60c07fc2270ed6ba457cee7765fd5888a0c801ae
Rob Frawley 2nd <rfrawley@scribenet.com>
2015-01-26 06:54:06 -0500

    non-interactive by default, sequence alterations

--- btrfs/btrfs-balance-all.bash
+++ btrfs/btrfs-balance-all.bash
@@ -17,9 +17,17 @@ source "${BOOTSTRAP_FILEPATH}"
 ## User Configuration
 ##
 btrfs_vols="/mnt/storage/ /mnt/main/ /mnt/volatile/ /mnt/web/"
-btrfs_increments=$(seq 3)
+btrfs_seq=3
 
+##
+## Internal Configuration
+##
+SELF_SCRIPT_NAME="Btrfs Filesystem Balance"
+OUT_PROMPT_DEFAULT="y"
+
+##
 ## Display welcome message
+##
 function out_welcome_custom 
 {
     out_lines \
@@ -28,11 +36,17 @@ function out_welcome_custom
         "Performs an incremental balance on all volumes."
 }
 
+##
 ## Check for any variant of -h|--help|-help|--h and display program usage
+##
 if [[ $(echo "$@" | grep -E -e "\-?\-h(elp)?\b") ]];
 then
     out_usage
 fi
+
+##
+## GO!
+##
 
 ## Welcome message
 out_welcome
@@ -44,7 +58,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 ## Check for required bins
-check_bins_and_setup_abs_path_vars btrfs wc
+check_bins_and_setup_abs_path_vars btrfs wc seq
 
 ## Show runtime configuration setup
 out_info_config \
@@ -52,33 +66,38 @@ out_info_config \
     "" \
     "  Btrfs Path -> ${bin_btrfs}" \
     "  Volumes    -> ${btrfs_vols}" \
-    "  Increments -> ${btrfs_increments}"
+    "  Increments -> 1 to ${btrfs_seq}"
 
 ## Allow the user to bail
 out_prompt_boolean "0" "Would you like to continue?" "Confirm Config" "y"
 
 ## Get final count of volumes to iterate over
+btrfs_increments="$(${bin_seq} ${btrfs_seq})"
 btrfs_vols_count="$(echo "${btrfs_vols}" | ${bin_wc} -w)"
+
+## Set stage to 1
+stage_i=1
 
 ## Foreach volume
 for volume in ${btrfs_vols}
 do
     ## User Output
     out_stage \
-        "1" \
+        "${stage_i}" \
         "${btrfs_vols_count}" \
         "Balancing: ${volume}"
 
     for btrfs_increment in ${btrfs_increments}
     do
-
         out_info \
-            "Running with durage value of ${btrfs_increment}." \
+            "Running: Filesystem balance on ${volume} with dusage of ${btrfs_increment}." \
             "Command: ${bin_btrfs} fi balance -dusage=${btrfs_increment} ${volume} > /dev/null 2>&1"
 
-        ${bin_btrfs} fi balance -dusage=${btrfs_increment} ${volume} > /dev/null 2>&1
-
+        ${bin_btrfs} fi balance start -dusage=${btrfs_increment} ${volume} > /dev/null 2>&1
     done
+
+    ## Increment stage
+    stage_i=$((stage_i + 1))
 
 done
 
